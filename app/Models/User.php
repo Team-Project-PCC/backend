8f5550d8690eb5afc771126db68c12dad7e2681c
Maulidaa <maulidaallya06@gmail.com>
2025-03-02 08:52:13 +0700

    Add url_avatar field to User model and migration for user profile images

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,7 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
         'email',
         'password',
         'email_verified_at',
+        'url_avatar',
     ];
 
     /**
